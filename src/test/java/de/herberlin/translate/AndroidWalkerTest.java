73a263dacd94be55355369c700f469e7e4ad7843
Hans-Joachim.HERBERT <herberlin@nexgo.de>
2021-06-01 09:26:45 +0200

    Android implementation

--- src/test/java/de/herberlin/translate/AndroidWalkerTest.java
+++ src/test/java/de/herberlin/translate/AndroidWalkerTest.java
@@ -6,9 +6,20 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AndroidWalkerTest {
     Translator translator = new DummyTranslator();
@@ -17,18 +28,74 @@ public class AndroidWalkerTest {
 
     @Before
     public void setUp() throws IOException {
-        File target= new File("target/data");
+        File target = new File("target/data");
         File source = new File("data");
         FileUtils.deleteDirectory(target);
         FileUtils.copyDirectory(source, target);
-     }
+    }
 
-    @org.junit.Test
-    public void translate() throws MojoExecutionException {
+    @Test
+    public void translateDe() throws MojoExecutionException, ParserConfigurationException, IOException, SAXException {
+        List<String> existingItems = Arrays.asList("warning", "service_unavailable", "kph");
+        File sourceFile = new File("target/data/android/values/strings.xml");
+        File deFile = new File("target/data/android/values-de/strings.xml");
         AndroidWalker testObj = new AndroidWalker();
-        testObj.init(translator, source, log );
-        testObj.translate("ru");
-        Assert.assertTrue(new File("target/data/android/values-ru/strings.xml").exists() );
+        testObj.init(translator, source, log);
+        testObj.translate("de");
+        Assert.assertTrue(deFile.exists());
 
+        Document sourceDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(sourceFile);
+        Document ruDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(deFile);
+        Map<String, Node> sourceMap = createMap(sourceDoc);
+        Map<String, Node> ruMap = createMap(ruDoc);
+        for (String key : sourceMap.keySet()) {
+            Node node = sourceMap.get(key);
+            if (node.getAttributes().getNamedItem("translatable") == null) {
+                Assert.assertTrue("Key not found: " + key, ruMap.containsKey(key));
+                if (existingItems.contains(key)) {
+                    Assert.assertEquals("Already existing:", "exiting", ruMap.get(key).getTextContent());
+                } else {
+                    Assert.assertEquals("No translation:", sourceMap.get(key).getTextContent() + "-de", ruMap.get(key).getTextContent());
+                }
+            } else {
+                Assert.assertFalse("Must not be here: " + key, ruMap.containsKey(key));
+            }
+        }
+    }
+
+    @Test
+    public void translateRu() throws MojoExecutionException, ParserConfigurationException, IOException, SAXException {
+        File sourceFile = new File("target/data/android/values/strings.xml");
+        File ruFile = new File("target/data/android/values-ru/strings.xml");
+        AndroidWalker testObj = new AndroidWalker();
+        testObj.init(translator, source, log);
+        testObj.translate("ru");
+        Assert.assertTrue(ruFile.exists());
+
+        Document sourceDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(sourceFile);
+        Document ruDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(ruFile);
+        Map<String, Node> sourceMap = createMap(sourceDoc);
+        Map<String, Node> ruMap = createMap(ruDoc);
+        for (String key : sourceMap.keySet()) {
+            Node node = sourceMap.get(key);
+            if (node.getAttributes().getNamedItem("translatable") == null) {
+                Assert.assertTrue("Key not found: " + key, ruMap.containsKey(key));
+                Assert.assertEquals("No translation:", sourceMap.get(key).getTextContent() + "-ru", ruMap.get(key).getTextContent());
+            } else {
+                Assert.assertFalse("Must not be here: " + key, ruMap.containsKey(key));
+            }
+        }
+    }
+
+
+    private Map<String, Node> createMap(Document target) {
+        Map<String, Node> result = new HashMap<>();
+        NodeList list = target.getElementsByTagName("string");
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            String nodeName = node.getAttributes().getNamedItem("name").getNodeValue();
+            result.put(nodeName, node);
+        }
+        return result;
     }
 }
